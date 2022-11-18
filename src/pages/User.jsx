4042344b11c8781e3ffe15755021e4e61ef5294e
daffadmr <daffaardhika@gmail.com>
2022-11-18 15:50:48 +0700

    Development: Fetch data

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import UserAPI from "../apis/users.api";
 
 const User = () => {
-  return (
-    <div>User</div>
-  )
-}
+  const [user, setUser] = useState([]);
 
-export default User
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await UserAPI.getAllUser();
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getUser();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Users</h1>
+      <div className="users">
+        {user.map((data) => {
+          return (
+            <div className="user-card">
+              <p>{data.username}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default User;
