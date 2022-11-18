35bd30be9864daa01ff4023b7bbbd18032ac177b
daffadmr <daffaardhika@gmail.com>
2022-11-18 15:50:19 +0700

    Development: Edit export

---
+++ src/configs/axiosInstance.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import CONST from "../utils/constants";
+
+const config = {
+  baseURL: CONST.BASE_URL,
+  // headers:{
+  //   'api-key': CONST.KEY
+  // }
+}
+
+const axiosInstance = axios.create(config)
+export default axiosInstance
