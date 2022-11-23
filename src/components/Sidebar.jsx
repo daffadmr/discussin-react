2fe2a273d03ad68899e0dd07b25296ca2a99c533
daffadmr <daffaardhika@gmail.com>
2022-11-23 22:28:25 +0700

    development: Add sidebar active list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import Logo from "../assets/svg/logo";
 import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
@@ -7,6 +7,31 @@ import TopicOutlinedIcon from "@mui/icons-material/TopicOutlined";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 const Sidebar = () => {
+  const [activeList, setActiveList] = useState(null);
+
+  const list = [
+    {
+      icon: GridViewOutlinedIcon,
+      route: "/",
+      name: "Dashboard",
+    },
+    {
+      icon: AccountCircleOutlinedIcon,
+      route: "/users",
+      name: "Users",
+    },
+    {
+      icon: TopicOutlinedIcon,
+      route: "/threads",
+      name: "Threads",
+    },
+    {
+      icon: TopicOutlinedIcon,
+      route: "/topics",
+      name: "Topic",
+    },
+  ];
+
   return (
     <div className="h-screen w-[224px] shadow-2xl">
       <div className="flex items-center justify-center p-[10px]">
@@ -14,22 +39,19 @@ const Sidebar = () => {
       </div>
       <div className="flex flex-col justify-between items-center h-[85vh]">
         <ul>
-          <li className="sidebar-list">
-            <GridViewOutlinedIcon />
-            <Link to={"/"}>Dashboard</Link>
-          </li>
-          <li className="sidebar-list">
-            <AccountCircleOutlinedIcon />
-            <Link to={"/users"}>Users</Link>
-          </li>
-          <li className="sidebar-list">
-            <GridViewOutlinedIcon />
-            <Link to={"/threads"}>Threads</Link>
-          </li>
-          <li className="sidebar-list">
-            <TopicOutlinedIcon />
-            <Link to={"/topics"}>Topics</Link>
-          </li>
+          {list.map((item) => {
+            return (
+              <li
+                onClick={() => setActiveList(item.name)}
+                className={`flex gap-2 pb-5 text-[16px] text-primary ${activeList === item.name && 'text-black'} ${activeList === null && window.location.pathname === item.route ? "text-black" : ""}`}
+              >
+                <Link to={`${item.route}`} className={`flex gap-2`}>
+                  <item.icon />
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
         <div>
           <button className="flex gap-2 text-[16px] text-primary">
