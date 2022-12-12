2839cada44596d0f2e3a0f81a24f947f946991fc
daffadmr <daffaardhika@gmail.com>
2022-12-12 18:47:04 +0700

    configure topic slice

--- src/store/features/topicSlice.js
+++ src/store/features/topicSlice.js
@@ -1,11 +1,11 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import TopicApi from "../../apis/topic.api";
 
-const initialState = {
-  data: [],
-  status: "idle",
-  error: null,
-};
+// const initialState = {
+//   data: [],
+//   status: "idle",
+//   error: null,
+// };
 
 export const fetchTopic = createAsyncThunk("fetch/topic", async () => {
   try {
@@ -36,32 +36,53 @@ export const deleteTopic = createAsyncThunk("delete/topic", async (id) => {
   }
 });
 
+const topicEntity = createEntityAdapter({
+  selectId: (topic) => topic.ID
+})
+
 const topicSlice = createSlice({
   name: "topic",
-  initialState,
-  extraReducers(builder) {
-    builder
-      .addCase(fetchTopic.pending, (state, action) => {
-        state.status = "loading";
-      })
-      .addCase(fetchTopic.fulfilled, (state, action) => {
-        state.status = "succeed";
-        state.data = action.payload;
-      })
-      .addCase(fetchTopic.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      })
-      .addCase(createTopic.fulfilled, (state, action) => {
-        state.status = "succeed";
-        state.data.push(action.payload.data);
-      })
-      .addCase(deleteTopic.fulfilled, (state, action) => {
-        state.status = "succeed";
-        console.log(action)
-        state.data = state.data.filter(val => val.ID !== action.meta.arg)
-      });
-  },
+  initialState: topicEntity.getInitialState(),
+  extraReducers: {
+    [fetchTopic.fulfilled]: (state, action) => {
+      topicEntity.setAll(state, action.payload);
+    },
+    [createTopic.fulfilled]: (state, action) => {
+      topicEntity.addOne(state, action.payload.data);
+    },
+    [deleteTopic.fulfilled]: (state, { payload: id }) => {
+      topicEntity.removeOne(state, id);
+      // topicEntity = state.data.filter(val => val.ID !== action.meta.arg)
+    },
+    // [updateProduct.fulfilled]: (state, action) => {
+    //   userEntity.updateOne(state, { id: action.payload.id, updates: action.payload });
+    // }
+  }
+  // initialState,
+  // extraReducers(builder) {
+  //   builder
+  //     .addCase(fetchTopic.pending, (state, action) => {
+  //       state.status = "loading";
+  //     })
+  //     .addCase(fetchTopic.fulfilled, (state, action) => {
+  //       state.status = "succeed";
+  //       state.data = action.payload;
+  //     })
+  //     .addCase(fetchTopic.rejected, (state, action) => {
+  //       state.status = "failed";
+  //       state.error = action.error.message;
+  //     })
+  //     .addCase(createTopic.fulfilled, (state, action) => {
+  //       state.status = "succeed";
+  //       state.data.push(action.payload.data);
+  //     })
+  //     .addCase(deleteTopic.fulfilled, (state, action) => {
+  //       state.status = "succeed";
+  //       console.log(action)
+  //       state.data = state.data.filter(val => val.ID !== action.meta.arg)
+  //     });
+  // },
 });
 
+export const topicSelector = topicEntity.getSelectors(state => state.topic)
 export default topicSlice.reducer;
