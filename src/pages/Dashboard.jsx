35f3fac65eb7f1c9dac762b4dcf1a76d45765638
daffadmr <daffaardhika@gmail.com>
2022-12-16 16:12:54 +0700

    configure new endpoint in dashboard home

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,25 +2,27 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 import TopicOutlinedIcon from "@mui/icons-material/TopicOutlined";
-import { fetchDataUser, userSelector } from "../store/features/userSlice";
-import { fetchTopic, topicSelector } from "../store/features/topicSlice";
+import axiosInstance from "../configs/axiosInstance";
+import { useState } from "react";
 
 const Dashboard = () => {
-  const users = useSelector(userSelector.selectAll);
-  const topics = useSelector(topicSelector.selectAll);
-
-  const dispatch = useDispatch();
-
-  const filteredUser = users.filter((user) => {
-    return !user.isAdmin;
-  });
+  const [dashboardData, setDashboardData] = useState({})
+  console.log(dashboardData)
+  const dashboardAPI = async () => {
+    try {
+      const response = await axiosInstance.get("dashboard");
+      console.log(response.data);
+      setDashboardData(response.data)
+      return response.data;
+    } catch(error) {
+      console.log(error)
+    }
+  };
 
   useEffect(() => {
-    dispatch(fetchDataUser());
-    dispatch(fetchTopic());
+    dashboardAPI();
   }, []);
 
-  console.log(users);
   return (
     <div className="px-12">
       <h1>Dashboard</h1>
@@ -28,7 +30,7 @@ const Dashboard = () => {
         <div className="dashboard-card">
           <div className="flex flex-col gap-3 justify-center">
             <h3 className="text-3xl">Users</h3>
-            <h3 className="text-3xl font-extrabold">{filteredUser.length}</h3>
+            <h3 className="text-3xl font-extrabold">{dashboardData.user_total}</h3>
           </div>
           <div className="flex items-center">
             <PeopleAltOutlinedIcon
@@ -41,8 +43,22 @@ const Dashboard = () => {
         </div>
         <div className="dashboard-card">
           <div className="flex flex-col gap-3 justify-center">
+            <h3 className="text-3xl">Post</h3>
+            <h3 className="text-3xl font-extrabold">{dashboardData.post_total}</h3>
+          </div>
+          <div className="flex items-center">
+            <TopicOutlinedIcon
+              color="white"
+              sx={{
+                fontSize: 60,
+              }}
+            />
+          </div>
+        </div>
+        <div className="dashboard-card">
+          <div className="flex flex-col gap-3 justify-center">
             <h3 className="text-3xl">Topics</h3>
-            <h3 className="text-3xl font-extrabold">{topics.length}</h3>
+            <h3 className="text-3xl font-extrabold">{dashboardData.topic_total}</h3>
           </div>
           <div className="flex items-center">
             <TopicOutlinedIcon
