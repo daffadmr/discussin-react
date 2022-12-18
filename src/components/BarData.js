0ae3ccfb317d6560546b5aec6df93086e481a197
Ramdhani11 <anwarramdhanie@gmail.com>
2022-12-18 08:20:36 +0700

    Chart

---
+++ src/components/BarData.js
@@ -0,0 +1,74 @@
+export const UserData = [
+  {
+    id: 1,
+    bulan: "Januari",
+    userLost: 15,
+    userGain: 62,
+  },
+  {
+    id: 2,
+    bulan: "Februari",
+    userLost: 15,
+    userGain: 70,
+  },
+  {
+    id: 2,
+    bulan: "Maret",
+    userLost: 15,
+    userGain: 82,
+  },
+  {
+    id: 4,
+    bulan: "April",
+    userLost: 15,
+    userGain: 64,
+  },
+  {
+    id: 5,
+    bulan: "Mei",
+    userLost: 15,
+    userGain: 73,
+  },
+  {
+    id: 6,
+    bulan: "Juni",
+    userLost: 15,
+    userGain: 71,
+  },
+  {
+    id: 7,
+    bulan: "July",
+    userLost: 15,
+    userGain: 91,
+  },
+  {
+    id: 8,
+    bulan: "Agustus",
+    userLost: 15,
+    userGain: 68,
+  },
+  {
+    id: 9,
+    bulan: "Oktober",
+    userLost: 15,
+    userGain: 102,
+  },
+  {
+    id: 10,
+    bulan: "Oktober",
+    userLost: 15,
+    userGain: 73,
+  },
+  {
+    id: 11,
+    bulan: "November",
+    userLost: 15,
+    userGain: 100,
+  },
+  {
+    id: 12,
+    bulan: "Desember",
+    userLost: 15,
+    userGain: 89,
+  },
+];
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,20 +5,31 @@ import TopicOutlinedIcon from "@mui/icons-material/TopicOutlined";
 import { TabTitle } from "../components/title";
 import axiosInstance from "../configs/axiosInstance";
 import { useState } from "react";
+import BarChart from "../components/BarChart";
+import { UserData } from "../components/BarData";
 
 const Dashboard = () => {
   TabTitle("Dashboard | discuss.in");
-  console.log(process.env.REACT_APP_BASE_URL)
-  const [dashboardData, setDashboardData] = useState({})
-  console.log(dashboardData)
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.bulan),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: ["#323C5C"],
+      },
+    ],
+  });
+  const [dashboardData, setDashboardData] = useState({});
+  console.log(dashboardData);
   const dashboardAPI = async () => {
     try {
       const response = await axiosInstance.get("dashboard");
       console.log(response.data);
-      setDashboardData(response.data)
+      setDashboardData(response.data);
       return response.data;
-    } catch(error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -27,49 +38,62 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="px-12">
-      <h1>Dashboard</h1>
-      <div className="flex text-white mt-5 gap-5">
-        <div className="dashboard-card">
-          <div className="flex flex-col gap-3 justify-center">
-            <h3 className="text-3xl">Users</h3>
-            <h3 className="text-3xl font-extrabold">{dashboardData.user_total}</h3>
+    <div className="container">
+      <h1 className="pb-5">Dashboard</h1>
+      <div className="w-[80vw] h-[90vh] rounded-lg ">
+        <div className="w-[80%]">
+          <div className="flex text-white mt-5 gap-5">
+            <div className="dashboard-card">
+              <div className="flex flex-col gap-3 justify-center">
+                <h3 className="text-3xl">Users</h3>
+                <h3 className="text-3xl font-extrabold">
+                  {dashboardData.user_total}
+                </h3>
+              </div>
+              <div className="flex items-center">
+                <PeopleAltOutlinedIcon
+                  color="white"
+                  sx={{
+                    fontSize: 60,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="dashboard-card">
+              <div className="flex flex-col gap-3 justify-center">
+                <h3 className="text-3xl">Post</h3>
+                <h3 className="text-3xl font-extrabold">
+                  {dashboardData.post_total}
+                </h3>
+              </div>
+              <div className="flex items-center">
+                <TopicOutlinedIcon
+                  color="white"
+                  sx={{
+                    fontSize: 60,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="dashboard-card">
+              <div className="flex flex-col gap-3 justify-center">
+                <h3 className="text-3xl">Topics</h3>
+                <h3 className="text-3xl font-extrabold">
+                  {dashboardData.topic_total}
+                </h3>
+              </div>
+              <div className="flex items-center">
+                <TopicOutlinedIcon
+                  color="white"
+                  sx={{
+                    fontSize: 60,
+                  }}
+                />
+              </div>
+            </div>
           </div>
-          <div className="flex items-center">
-            <PeopleAltOutlinedIcon
-              color="white"
-              sx={{
-                fontSize: 60,
-              }}
-            />
-          </div>
-        </div>
-        <div className="dashboard-card">
-          <div className="flex flex-col gap-3 justify-center">
-            <h3 className="text-3xl">Post</h3>
-            <h3 className="text-3xl font-extrabold">{dashboardData.post_total}</h3>
-          </div>
-          <div className="flex items-center">
-            <TopicOutlinedIcon
-              color="white"
-              sx={{
-                fontSize: 60,
-              }}
-            />
-          </div>
-        </div>
-        <div className="dashboard-card">
-          <div className="flex flex-col gap-3 justify-center">
-            <h3 className="text-3xl">Topics</h3>
-            <h3 className="text-3xl font-extrabold">{dashboardData.topic_total}</h3>
-          </div>
-          <div className="flex items-center">
-            <TopicOutlinedIcon
-              color="white"
-              sx={{
-                fontSize: 60,
-              }}
-            />
+          <div>
+            <BarChart chartData={userData} />
           </div>
         </div>
       </div>
