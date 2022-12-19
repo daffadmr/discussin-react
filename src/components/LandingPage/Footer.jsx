ce80c663de012e9ac3324122959004ffce5549e3
daffadmr <daffaardhika@gmail.com>
2022-12-20 02:42:12 +0700

    add sign in button for admin in footer

--- src/components/LandingPage/Footer.jsx
+++ src/components/LandingPage/Footer.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LogoWithName from "../../assets/svg/LogoWithName";
+import auth from "../../utils/auth";
 
 const Footer = () => {
   return (
     <footer className="bg-[#301C8A] text-white">
       <div className="container">
         <div className="py-12 flex flex-col xl:flex-row justify-between flex-wrap gap-16">
-        <LogoWithName/>
+          <div className="flex flex-col gap-2">
+            <LogoWithName />
+            {auth.isAuthorization() ? (
+              <div className="flex flex-col">
+                <Link to={"/dashboard"}>Go to Dashboard</Link>
+                <span
+                  className="p-0 m-0"
+                  onClick={() => {
+                    auth.signOut();
+                  }}
+                >
+                  Log out
+                </span>
+              </div>
+            ) : (
+              <Link to={"/login"}>
+                <span className="underline"> Admin Page</span>
+              </Link>
+            )}
+          </div>
           <div className="flex gap-16 xl:gap-52 flex-col xl:flex-row flex-wrap">
             <div className="flex flex-col">
               <h3 className="font-bold text-lg">About</h3>
@@ -33,8 +54,8 @@ const Footer = () => {
         </div>
       </div>
       <div className="flex justify-center items-center py-4 bg-[#373F41] text-white">
-          <p>© Diskusi.In. All rights reserved.</p>
-        </div>
+        <p>© Discuss.In. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
