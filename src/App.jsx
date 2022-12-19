7a0ef52bf5ff5fab13b4809388e2097c1ae0c541
daffadmr <daffaardhika@gmail.com>
2022-12-20 02:35:03 +0700

    configure react helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import "./tailwind.css"
+import "./tailwind.css";
 import SetupRouters from "./routes/SetupRouters";
+import { HelmetProvider } from "react-helmet-async";
 
-class App extends Component {
-  render() {
-    return (
+
+const App = () => {
+  const helmetContext = {};
+  return (
+    <HelmetProvider context={helmetContext}>
       <div className="App">
-          <SetupRouters />
-      </div>  
-    );
-  }
+        <SetupRouters />
+      </div>
+    </HelmetProvider>
+  );
 }
 
-export default App;
+export default App
